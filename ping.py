340407c4471c8bf952dce9fcb27bf681408187d8
jaygalvin <sgtgalvin@gmail.com>
2025-02-26 11:59:17 -0800

    add ping.py

---
+++ ping.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+from soco import SoCo
+import time
+from netdisco.discovery import NetworkDiscovery
+import urllib2
+
+netdis = NetworkDiscovery()
+
+netdis.scan()
+
+for dev in netdis.discover():
+    if dev == 'sonos':
+        for player in netdis.get_info(dev):
+            print(SoCo(player).ip_address)
+
+netdis.stop()
